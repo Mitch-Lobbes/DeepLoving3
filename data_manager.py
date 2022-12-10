4032de4d2fa24e1157e1121087bf09ff51a07da7
PaoFe <p.s.feil@student.vu.nl>
2022-12-10 09:44:35 +0100

    Q9 and Q10

--- data_manager.py
+++ data_manager.py
@@ -21,27 +21,35 @@ class DatasetFromSubset(Dataset):
 
 
 class DataManager:
-    def __init__(self, dataset: str, batch_size: int, train_size: int = 50000, val_size: int = 10000, transform=False) -> None:
+    def __init__(self, dataset: str, batch_size: int, train_size: int = 50000, val_size: int = 10000, test_size: int = 10000, transform=False) -> None:
         if dataset == 'mnist' and transform == False:
             train_val = torchvision.datasets.MNIST(root='data', train=True, download=True, transform=transforms.ToTensor())
             train, val = torch.utils.data.random_split(train_val, [train_size, val_size])
             test = torchvision.datasets.MNIST(root='data', train=False, download=True, transform=transforms.ToTensor())
 
-
-        if dataset == "mnist" and transform == True:
+        # Setup for question 9
+        elif dataset == "mnist" and transform == True:
             test = torchvision.datasets.MNIST(root='data', train=False, download=True, transform=transforms.ToTensor())
             train_val = torchvision.datasets.MNIST(root='data', train=True, download=True, transform=transforms.ToTensor())
             train, val = torch.utils.data.random_split(train_val, [train_size, val_size])
             split_1, split_2, split_3, split_4, split_5, remain_train = torch.utils.data.random_split(train, [1000, 1000, 1000, 1000, 1000, 45000])
 
-            augm_1 = DatasetFromSubset(subset=split_1, transform=transformation(split=1))
-            augm_2 = DatasetFromSubset(subset=split_2, transform=transformation(split=2))
-            augm_3 = DatasetFromSubset(subset=split_3, transform=transformation(split=3))
-            augm_4 = DatasetFromSubset(subset=split_4, transform=transformation(split=4))
-            augm_5 = DatasetFromSubset(subset=split_5, transform=transformation(split=5))
+            augm_1 = DatasetFromSubset(subset=split_1, transform=transformation(type=9.1))
+            augm_2 = DatasetFromSubset(subset=split_2, transform=transformation(type=9.2))
+            augm_3 = DatasetFromSubset(subset=split_3, transform=transformation(type=9.3))
+            augm_4 = DatasetFromSubset(subset=split_4, transform=transformation(type=9.4))
+            augm_5 = DatasetFromSubset(subset=split_5, transform=transformation(type=9.5))
 
             train = torch.utils.data.ConcatDataset([augm_1, augm_2, augm_3, augm_4, augm_5, train])
-            
+        
+        # Setup for question 10
+        elif dataset == "mnist-varres" and transform == True:
+            all_data = torchvision.datasets.ImageFolder(root='data', transform=transformation(type=10))
+            train, val, test = torch.utils.data.random_split(all_data, [train_size, val_size, test_size])
+        
+        else:
+            raise Exception("This setup for the DataManager is not supported.")
+
         self.batch_size = batch_size
         self.trainloader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=True, num_workers=2)
         self.valloader = torch.utils.data.DataLoader(val, batch_size=batch_size, shuffle=True, num_workers=2)
--- transformations.py
+++ transformations.py
@@ -1,20 +1,27 @@
 import torchvision.transforms as transforms
 
 
-def transformation(split):
-    if split == 1:
+def transformation(type):
+
+    # Transformations for question 9
+    if type == 9.1:
         transformation = transforms.CenterCrop(18)
-    elif split == 2:
+    elif type == 9.2:
         transformation = transforms.CenterCrop(128)
-    elif split == 3:
+    elif type == 9.3:
         transformation = transforms.ColorJitter(brightness=0.50, contrast=0.50, saturation=0.05, hue=0.05)
-    elif split == 4:
+    elif type == 9.4:
         transformation = transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5))
-    elif split == 5:
+    elif type == 9.5:
         transformation = transforms.RandomRotation(degrees=45)
+
+    # Transformation for question 10
+    elif type == 10:
+        transformation = transforms.Resize(size=(28,28))
     else:
-        raise Exception("Only 5 different transformations are implemented.")
+        raise Exception("Only 6 different transformations are implemented.")
     
+    # Setup relevant transformation pipeline
     transform = transforms.Compose([
                 #transforms.ToPILImage(),
                 transformation,
